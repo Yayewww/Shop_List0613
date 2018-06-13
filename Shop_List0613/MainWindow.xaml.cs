c5e0bc29e6be5e21ccdf3ed7109cf225f2f3f9b2
YAYEWWW\Yayewww <knig80005@gmail.com>
2018-06-14 07:43:50 +0800

    Yayewww
    嗚嗚嗚，加總解決不了。
    煩ㄟ

--- Shop_List0613/MainWindow.xaml.cs
+++ Shop_List0613/MainWindow.xaml.cs
@@ -20,9 +20,138 @@ namespace Shop_List0613
     /// </summary>
     public partial class MainWindow : Window
     {
+        //給予總和初值。
+        public int Total_Cost = 0;
+        public int item_Cost = 0;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        //按下關閉視窗
+        private void CloseBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        //按住拖曳視窗
+        private void BaseGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        //按下新增項目
+        private void Add_Btn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Shop_item _Item = new Shop_item();
+            _Item.item_Content = "New";
+            _Item.DeleteItem += new EventHandler(DeleteItem);
+            _Item.PlusAll += new EventHandler(PlusAll);
+
+            //放入清單中
+            TaskPanel.Children.Add(_Item);
+
+            //加總費用
+            int item_Cost = int.Parse(_Item.item_value);
+            Total_Cost += item_Cost;
+            Cost_Number.Text = Total_Cost.ToString();
+        }
+
+        //輸入錢
+        private void PlusAll(object sender, EventArgs e)
+        {
+            CaoMoney();
+        }
+
+
+        // 刪除事件
+        private void DeleteItem(object sender, EventArgs e)
+        {
+            //移除項目
+            TaskPanel.Children.Remove((Shop_item)sender);
+            //算錢
+            CaoMoney();
+        }
+
+        //關閉視窗
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            //將項目文字存成文字檔
+            string datas = "";
+            foreach(Shop_item _Item in TaskPanel.Children)
+            {
+                datas += _Item.item_Date + "|" + _Item.item_Content+ ","+_Item.item_value + "\r\n";
+            }
+
+            System.IO.File.WriteAllText(@"C:\Buy_temp\data.txt", datas);
+        }
+
+        //開啟視窗後讀取事件
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            //開檔
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Buy_temp\data.txt");         
+            //讀取每一行
+            foreach (string line in lines)
+            {
+                //用" | " 符號拆開
+                string[] parts = line.Split('|',',');//將"|"前後分成[0]跟[1]","分[2]
+
+                //建立Shop_item
+                Shop_item _Item = new Shop_item();
+                _Item.item_Date = parts[0];
+                _Item.item_Content = parts[1];
+                _Item.item_value = parts[2];
+                _Item.DeleteItem += new EventHandler(DeleteItem);
+                _Item.PlusAll += new EventHandler(PlusAll);
+                //放到清單中
+                TaskPanel.Children.Add(_Item);
+
+                //將費用文字轉換成數值
+                int item_Cost = int.Parse(_Item.item_value);
+                Total_Cost += item_Cost;
+                Cost_Number.Text = Total_Cost.ToString();
+            }
+            
+        }
+
+        void CaoMoney()
+        {
+            //將項目文字存成文字檔
+            string datas = "";
+            foreach (Shop_item _Item in TaskPanel.Children)
+            {
+                datas += _Item.item_Date + "|" + _Item.item_Content + "," + _Item.item_value + "\r\n";
+            }
+
+            System.IO.File.WriteAllText(@"C:\Buy_temp\data.txt", datas);
+
+            //開檔
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Buy_temp\data.txt");
+            //讀取每一行
+            foreach (string line in lines)
+            {
+                //用" | " 符號拆開
+                string[] parts = line.Split('|', ',');//將"|"前後分成[0]跟[1]","分[2]
+
+                //建立Shop_item
+                Shop_item _Item = new Shop_item();
+                _Item.item_Date = parts[0];
+                _Item.item_Content = parts[1];
+                _Item.item_value = parts[2];
+                _Item.DeleteItem += new EventHandler(DeleteItem);
+                _Item.PlusAll += new EventHandler(PlusAll);
+
+                //將每一行的value 轉成int*****BUGBOYS
+                 Total_Cost += int.Parse(_Item.item_value);
+                
+                
+            }
+            
+            Cost_Number.Text = Total_Cost.ToString();
+        }
+
     }
 }
---
+++ Shop_List0613/Shop_item.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shop_List0613
+{
+    /// <summary>
+    /// Shop_item.xaml 的互動邏輯
+    /// </summary>
+    public partial class Shop_item : UserControl
+    {
+        public int item_coco = 0;
+
+        public Shop_item()
+        {
+            InitializeComponent();
+        }
+
+        // 自訂刪除事件
+        public event EventHandler DeleteItem;
+        //自訂算錢事件
+        public event EventHandler PlusAll;
+
+        //封裝屬性 : 控制項內容文字
+        public string item_Content
+        {
+            get
+            {
+                return Content_Box.Text;
+            }
+            set
+            {
+                Content_Box.Text = value;
+            }
+        }
+
+        //加總文字項，按下Enter執行 計算加總事件
+        private void Cost_Box_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Cost_Box.Text != "" && e.Key == Key.Enter)
+            {
+                PlusAll(this, null);
+            }
+        }
+
+        //取得費用
+        public string item_value
+        {
+            get
+            {
+                return Cost_Box.Text;
+            }
+            set
+            {
+                Cost_Box.Text = value;
+            }
+        }
+        //封裝屬性 : 日期
+        public string item_Date
+        {
+            get
+            {
+                return Date_Box.Text;
+            }
+            set
+            {
+                Date_Box.Text = value;
+            }
+        }
+        
+
+        
+        //內容文字項，按下事件
+        private void Content_Box_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            //當內容空白，且按下Backspace時，引發DeleteItem事件。
+            if(Content_Box.Text =="" && e.Key == Key.Back)
+            {
+                DeleteItem(this, null);
+            }
+        }
+    }
+}
